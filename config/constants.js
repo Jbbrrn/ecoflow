dac9790725eb2e3bd8cf2f3b60d0b551e7611996
jbbrrn <jobbarrion2@gmail.com>
2025-12-22 15:30:51 +0800

    Update constants.js

--- config/constants.js
+++ config/constants.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-    JWT_SECRET: process.env.JWT_SECRET || 'YOUR_FALLBACK_SECRET_KEY_ECOFLOW',
-    DEVICE_API_KEY: process.env.DEVICE_API_KEY || 'YOUR_RPi_SECRET_KEY_123',
+    JWT_SECRET: process.env.JWT_SECRET || 'ecoflow_jwt_capstone9fA3Kx!Qm7R2@WZpE8cN',
+    DEVICE_API_KEY: process.env.DEVICE_API_KEY || 'group4_thesis_secret54rg79j32k4dsn930ytt26',
     SALT_ROUNDS: 10,
     PORT: process.env.PORT || 3000
 };
