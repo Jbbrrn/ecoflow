e9f9c9276a6a12718bce96a1ccf5bd210cccd55f
jbbrrn <jobbarrion2@gmail.com>
2025-12-26 14:11:06 +0800

    remove hardcoded secrets

--- config/constants.js
+++ config/constants.js
@@ -1,9 +1,17 @@
 require('dotenv').config();
 
+
+if (!process.env.JWT_SECRET) {
+    throw new Error('JWT_SECRET environment variable is required! Set it in .env file or environment variables.');
+}
+
+if (!process.env.DEVICE_API_KEY) {
+    throw new Error('DEVICE_API_KEY environment variable is required! Set it in .env file or environment variables.');
+}
+
 module.exports = {
-    JWT_SECRET: process.env.JWT_SECRET || 'ecoflow_jwt_capstone9fA3Kx!Qm7R2@WZpE8cN',
-    DEVICE_API_KEY: process.env.DEVICE_API_KEY || 'group4_thesis_secret54rg79j32k4dsn930ytt26',
+    JWT_SECRET: process.env.JWT_SECRET,
+    DEVICE_API_KEY: process.env.DEVICE_API_KEY,
     SALT_ROUNDS: 10,
     PORT: process.env.PORT || 3000
 };
-
